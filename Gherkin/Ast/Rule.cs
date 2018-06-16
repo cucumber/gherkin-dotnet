da2af5b69336dbbbfab9111ba8a5bbd8540e3532
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2018-06-16 20:38:09 +0100

    Implement Rule for C#,Java,JavaScript,Ruby and partially Go

--- Gherkin/Ast/Rule.cs
+++ Gherkin/Ast/Rule.cs
@@ -2,21 +2,17 @@
 
 namespace Gherkin.Ast
 {
-    public class Feature : IHasLocation, IHasDescription, IHasTags, IHasChildren
+    public class Rule : IHasLocation, IHasDescription, IHasChildren
     {
-        public IEnumerable<Tag> Tags { get; private set; }
         public Location Location { get; private set; }
-        public string Language { get; private set; }
         public string Keyword { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public IEnumerable<IHasLocation> Children { get; private set; }
 
-        public Feature(Tag[] tags, Location location, string language, string keyword, string name, string description, IHasLocation[] children)
+        public Rule(Location location, string keyword, string name, string description, IHasLocation[] children)
         {
-            Tags = tags;
             Location = location;
-            Language = language;
             Keyword = keyword;
             Name = name;
             Description = description;
